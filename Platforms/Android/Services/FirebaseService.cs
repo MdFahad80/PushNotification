445c5ee8c1576fb15ee688f97552d7f645a078d7
Md Fahad <fahad.programmer.me@outlook.com>
2024-11-19 09:11:09 +0600

    Add project files.

---
+++ Platforms/Android/Services/FirebaseService.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Firebase.Messaging;
+
+namespace PushNotification.Platforms.Android.Services
+{
+    [Service(Exported = true)]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirebaseService : FirebaseMessagingService
+    {
+        public FirebaseService() { }
+
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+            if (Preferences.ContainsKey("DeviceToken")) {
+                Preferences.Remove("DeviceToken");    
+            }
+            Preferences.Set("DeviceToken", token);
+        }
+
+    }
+}
